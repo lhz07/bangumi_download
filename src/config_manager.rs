1efa95d9c282005ae1453e4cb0f9a016fd533bca
lhz <a2722764368@gmail.com>
2025-04-06 17:54:04 +0800

    add more type and command of Message
    1. verify config when launch
    2. storage config in memory, so we just need read it from file only once
    3. add 4 types of msg cmd and 3 types of msg
    4. impl these function in modify_config()

--- src/config_manager.rs
+++ src/config_manager.rs
@@ -1,30 +1,58 @@
 use once_cell::sync::Lazy;
 use serde_json::Value;
-use sha2::{Digest, Sha256};
 use std::{error::Error, path::Path, vec};
 use tokio::{
     fs::{self},
     sync::{RwLock, mpsc},
 };
 
-use crate::cloud_manager::update_cloud_cookies;
+use crate::{
+    COOKIE_WRITE, ERROR_STATUS,
+    alist_manager::{get_alist_name_passwd, get_alist_token},
+    cloud_manager::update_cloud_cookies,
+};
 
 pub static CONFIG: Lazy<RwLock<Config>> = Lazy::new(|| RwLock::new(Config::new()));
 
 #[derive(Debug)]
 pub struct Message {
     keys: Vec<String>,
-    value: String,
-    append: bool,
+    value: MessageType,
+    cmd: MessageCmd,
+}
+
+#[derive(Debug)]
+pub enum MessageType {
+    Text(String),
+    List(Vec<String>),
+    Map(serde_json::Map<String, Value>),
+    None,
+}
+
+#[derive(Debug)]
+pub enum MessageCmd {
+    Append,
+    DeleteKey,
+    DeleteValue,
+    Replace,
+}
+
+impl MessageType {
+    pub fn to_value(self) -> Value {
+        match self {
+            Self::Text(text) => Value::String(text),
+            Self::List(list) => {
+                Value::Array(list.into_iter().map(|item| Value::String(item)).collect())
+            }
+            Self::Map(map) => Value::Object(map),
+            Self::None => Value::Null,
+        }
+    }
 }
 
 impl Message {
-    pub fn new(keys: Vec<String>, value: String, append: bool) -> Self {
-        Self {
-            keys,
-            value,
-            append,
-        }
+    pub fn new(keys: Vec<String>, value: MessageType, cmd: MessageCmd) -> Self {
+        Self { keys, value, cmd }
     }
 }
 
@@ -35,8 +63,37 @@ pub struct Config {
 
 impl Config {
     fn new() -> Self {
+        let data = Value::Null;
+        Config { data }
+    }
+
+    fn check_valid(json: &Value) -> Option<()> {
+        json["user"]["name"].is_string().then_some(())?;
+        json["bangumi"].is_object().then_some(())?;
+        json["cookies"].is_string().then_some(())?;
+        json["downloading_hash"].is_array().then_some(())?;
+        json["files_to_download"].is_object().then_some(())?;
+        json["filter"]["default"].is_array().then_some(())?;
+        json["rss_links"].is_object().then_some(())?;
+        json["temp"].is_object().then_some(())?;
+
+        Some(())
+    }
+
+    pub fn get_value(&self) -> &Value {
+        &self.data
+    }
+
+    pub fn get_mut_value(&mut self) -> &mut Value {
+        &mut self.data
+    }
+
+    pub async fn initial_config() -> Result<(), Box<dyn Error>> {
         let path = Path::new("config.json");
-        let old_json = std::fs::read_to_string(path).expect("can not read config.json");
+        let mut old_json = String::new();
+        if path.exists() {
+            old_json = std::fs::read_to_string(path).expect("can not read config.json");
+        }
         let data = if path.exists() && !old_json.is_empty() {
             let json = serde_json::from_str(&old_json).unwrap_or_else(|error| {
                 eprintln!(
@@ -53,18 +110,9 @@ impl Config {
             }
         } else {
             // get username and password
-            println!("Username:");
-            let mut name = String::new();
-            std::io::stdin().read_line(&mut name).expect("Failed to read username!");
-            name = name.trim().to_string();
-            let mut hasher = Sha256::new();
-            println!("Password:");
-            hasher.update(rpassword::read_password().expect("Failed to read password") + "-https://github.com/alist-org/alist");
-            let password = hex::encode(hasher.finalize());
-            let cookies = tokio::runtime::Runtime::new().expect("Can not start thread!").block_on(async{
-                update_cloud_cookies().await
-            });
-            let default_config = serde_json::json!({"user":{"name":name, "password": password},"bangumi":{}, "cookies": cookies, "rss_links": {}, "filter": {"611": ["内封"], "583": ["CHT"], "570": ["内封"], "default": ["简繁日内封", "简日内封", "简繁内封", "简体", "简日", "简繁日", "简中", "CHS"]}, "downloading": {}, "temp": {}, "files_to_download": {}});
+            let (name, password) = get_alist_name_passwd().await;
+            let cookies = update_cloud_cookies().await;
+            let default_config = serde_json::json!({"user":{"name":name, "password": password},"bangumi":{}, "cookies": cookies, "rss_links": {}, "filter": {"611": ["内封"], "583": ["CHT"], "570": ["内封"], "default": ["简繁日内封", "简日内封", "简繁内封", "简体", "简日", "简繁日", "简中", "CHS"]}, "magnets":{}, "downloading_hash": [], "hash_ani": {}, "temp": {}, "files_to_download": {}});
             let default_json = serde_json::to_string_pretty(&default_config).unwrap();
             std::fs::write(path, default_json).unwrap_or_else(|error| {
                 eprintln!("Can not write to path!\nError: {error}");
@@ -72,72 +120,92 @@ impl Config {
             });
             default_config
         };
-        Config { data }
-    }
-
-    fn check_valid(json: &Value) -> Option<()> {
-        json["user"]["name"].is_string().then_some(())?;
-        json["bangumi"].is_object().then_some(())?;
-        json["cookies"].is_object().then_some(())?;
-        json["downloading"].is_object().then_some(())?;
-        json["files_to_download"].is_object().then_some(())?;
-        json["filter"]["default"].is_array().then_some(())?;
-        json["rss_links"].is_object().then_some(())?;
-        json["temp"].is_object().then_some(())?;
-
-        Some(())
-    }
-
-    pub fn get_value(&self) -> &Value {
-        &self.data
-    }
-
-    pub fn get_mut_value(&mut self) -> &mut Value {
-        &mut self.data
+        *CONFIG.write().await = Config { data };
+        Ok(())
     }
 }
 
-pub fn initial_config() -> Result<(), Box<dyn Error>> {
-    futures::executor::block_on(async {
-        let _ = CONFIG.read().await;
-    });
-
-    Ok(())
-}
-
-pub async fn modify_config(mut rx: mpsc::UnboundedReceiver<Message>) -> Result<(), Box<dyn Error>> {
+pub async fn modify_config(mut rx: mpsc::UnboundedReceiver<Message>) {
     while let Some(msg) = rx.recv().await {
         let mut new_config = CONFIG.read().await.get_value().clone();
         let mut current = &mut new_config;
-        for key in msg.keys.iter() {
-            match current.get(key) {
-                Some(_) => {
-                    current = current.get_mut(key).unwrap();
-                    continue;
-                }
-                None => {
-                    current[key] = Value::Object(Default::default());
-                    current = current.get_mut(key).unwrap();
-                }
-            }
-        }
-        if msg.append {
-            if current.is_array() {
-                let arr = current.as_array_mut().expect("can not find the vec!");
-                arr.push(Value::String(msg.value));
-            } else {
-                let new_arr = vec![current.clone()];
-                *current = Value::Array(new_arr);
+        if let MessageCmd::DeleteKey = msg.cmd {
+            for key in msg.keys.iter().take(msg.keys.len() - 1) {
+                current = current.get_mut(key).unwrap();
             }
         } else {
-            *current = Value::String(msg.value);
+            for key in msg.keys.iter() {
+                match current.get(key) {
+                    Some(_) => {
+                        current = current.get_mut(key).unwrap();
+                        continue;
+                    }
+                    None => {
+                        current[key] = Value::Object(Default::default());
+                        current = current.get_mut(key).unwrap();
+                    }
+                }
+            }
         }
+        match msg.cmd {
+            MessageCmd::Append => match msg.value {
+                MessageType::Map(mut value) => {
+                    let map = current.as_object_mut().expect("can not find map!");
+                    map.append(&mut value);
+                }
+                MessageType::List(_) => {
+                    if !current.is_array() {
+                        *current = Value::Array(Default::default());
+                    }
+                    let arr = current.as_array_mut().expect("can not find the vec!");
+                    arr.append(msg.value.to_value().as_array_mut().unwrap());
+                }
+                MessageType::Text(_) => {
+                    if !current.is_array() {
+                        *current = Value::Array(Default::default());
+                    }
+                    let arr = current.as_array_mut().expect("can not find the vec!");
+                    arr.push(msg.value.to_value());
+                }
+                MessageType::None => (),
+            },
+            MessageCmd::DeleteKey => {
+                current
+                    .as_object_mut()
+                    .unwrap()
+                    .remove(msg.keys.last().unwrap());
+            }
+            MessageCmd::DeleteValue => {
+                let arr = current.as_array_mut().unwrap();
+                let del_value = msg.value.to_value();
+                if let Some(index) = arr.iter().position(|item| *item == del_value) {
+                    arr.remove(index);
+                }
+            }
+            MessageCmd::Replace => {
+                *current = msg.value.to_value();
+            }
+        }
+        println!("try to write");
         *CONFIG.write().await.get_mut_value() = new_config.clone();
+        println!("wrote!");
+        if msg.keys.first().unwrap() == "cookies" {
+            COOKIE_WRITE.notify_one();
+            println!("notify the thread");
+        }
+        #[cfg(not(test))]
+        {
         let new_config =
             serde_json::to_string_pretty(&new_config).expect("can not serialize new config");
         fs::write("config.json", new_config)
             .await
             .expect("can not write new config");
+        }
     }
-    Ok(())
+    #[cfg(not(test))]
+    if *ERROR_STATUS.read().await {
+        std::process::exit(1);
+    } else {
+        std::process::exit(0);
+    }
 }
