19b1c1fdee44339785a363b3e7e4b3034a49f41c
lhz <a2722764368@gmail.com>
2025-04-28 10:29:48 +0800

    Enhance safety and add functionality
    1. use anyhow to deal with errors
    2. use AtomicI32 instead of Cell
    3. have rss_receive check for undownloaded magnet links and attempt to download them.
    4. wait for config to be written before restarting refresh_download

--- src/update_rss.rs
+++ src/update_rss.rs
@@ -1,38 +1,36 @@
 use crate::{
-    CLIENT, CLIENT_WITH_RETRY, ERROR_STATUS, TX,
-    cloud_manager::cloud_download,
-    config_manager::{CONFIG, Message, MessageCmd, MessageType, modify_config},
+    cloud_manager::cloud_download, config_manager::{Message, MessageCmd, MessageType, CONFIG}, main_proc::{refresh_download, restart_refresh_download}, CLIENT_WITH_RETRY, ERROR_STATUS, TX
 };
 use futures::future::{self, join_all};
-use reqwest::Client;
 use reqwest_middleware::ClientWithMiddleware;
 use scraper::{Element, Html, Selector};
 use serde_json::{Value, json};
-use std::{cell::Cell, error::Error, pin::Pin, time::Duration, vec};
-use tokio::sync::mpsc;
-use tokio_retry::{
-    Retry,
-    strategy::{self, ExponentialBackoff, FibonacciBackoff},
+use std::{
+    sync::{Arc, atomic::AtomicI32},
+    vec,
 };
+use tokio::sync::{mpsc, Notify};
+use tokio_retry::{Retry, strategy::FibonacciBackoff};
 
 async fn get_response_text(
     url: &str,
     client: ClientWithMiddleware,
-) -> Result<String, Box<dyn Error>> {
+) -> Result<String, anyhow::Error> {
     let response = client.get(url).send().await?;
     if response.status().is_success() {
         Ok(response.text().await?)
     } else {
-        Err(format!("Request failed with status: {}", response.status()).into())
+        Err(anyhow::format_err!(
+            "Request failed with status: {}",
+            response.status()
+        ))
     }
 }
 
 pub async fn start_rss_receive(urls: Vec<&str>) {
-    // create client
     // read the config
     let old_config: Value = CONFIG.read().await.get_value().clone();
     // create the sender futures
-    // let mut futs: Vec<Pin<Box<dyn Future<Output = Result<(), Box<dyn Error>>>>>> = Vec::new();
     let mut futs = Vec::new();
     for url in urls {
         let tx = match TX.read().await.clone() {
@@ -41,11 +39,8 @@ pub async fn start_rss_receive(urls: Vec<&str>) {
         };
         futs.push(rss_receive(tx, url, &old_config, CLIENT_WITH_RETRY.clone()));
     }
-    // create the receiver future
-    // futs.push(Box::pin(modify_config(rx)));
     // get the results
     let results = future::join_all(futs).await;
-
     for result in results {
         if let Err(error) = result {
             eprintln!("{}", error);
@@ -119,16 +114,7 @@ pub async fn get_all_episode_magnet_links(
     let soup = Html::parse_document(&response);
     let selector = Selector::parse("a.magnet-link-wrap").unwrap();
     let elements = soup.select(&selector);
-    // for i in elements{
-    //     println!("{}", i.text().collect::<String>());
-    //     println!("{}", i.next_sibling_element()?.value().attr("data-clipboard-text")?);
-    // }
     let mut items = Vec::<Value>::new();
-    // let items = elements.map(|element|{
-    //     let title = element.text().collect::<String>();
-    //     let link = element.next_sibling_element()?.value().attr("data-clipboard-text")?;
-    //     json!({"title": title, "link": link});
-    // });
     for element in elements {
         items.push(json!({"title": element.text().collect::<String>(), 
                         "link": element.next_sibling_element()?.value().attr("data-clipboard-text")?}));
@@ -137,7 +123,6 @@ pub async fn get_all_episode_magnet_links(
         .iter()
         .map(|link| link.to_string())
         .collect();
-    // println!("{:?}", magnet_links);
     Some(magnet_links)
 }
 
@@ -172,21 +157,19 @@ pub async fn get_all_magnet(item_urls: Vec<&str>) -> Result<Vec<String>, &str> {
             }
         }
     }
-    // println!("return the links");
     Ok(magnet_links)
-    // println!("{:?}", results);
 }
 
 pub async fn get_a_magnet_link(url: &str) -> Option<String> {
-    let try_times = Cell::new(0);
+    let try_times = AtomicI32::new(0);
     let response = match Retry::spawn(FibonacciBackoff::from_millis(5000).take(3), async || {
-        if try_times.get() > 0 {
+        if try_times.load(std::sync::atomic::Ordering::SeqCst) > 0 {
             eprintln!("can not open {url}, waiting for retry.");
         }
-        try_times.set(1);
+        try_times.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
         let client = CLIENT_WITH_RETRY.clone();
         let res = get_response_text(url, client).await?;
-        Ok::<String, Box<dyn Error>>(res)
+        Ok::<String, anyhow::Error>(res)
     })
     .await
     {
@@ -212,13 +195,16 @@ pub async fn rss_receive(
     url: &str,
     old_config: &Value,
     client: ClientWithMiddleware,
-) -> Result<(), Box<dyn Error>> {
+) -> Result<(), anyhow::Error> {
     let response = client.get(url).send().await?.text().await?;
-    let rss_content = feedparser::from_str(&response).ok_or("can not parse rss!")?;
-    // println!("{:?}", rss_content);
-    // println!("{:?}", rss_content["rss"]["channel"]["item"].get(0));
-    let items = rss_content.items().ok_or("can not found items!")?;
-    let latest_item = items.first().ok_or("can not found latest item!")?;
+    let rss_content =
+        feedparser::from_str(&response).ok_or_else(|| anyhow::Error::msg("can not parse rss!"))?;
+    let items = rss_content
+        .items()
+        .ok_or_else(|| anyhow::Error::msg("can not found items!"))?;
+    let latest_item = items
+        .first()
+        .ok_or_else(|| anyhow::Error::msg("can not found latest item!"))?;
     let mut split_ani_sub = rss_content["link"]
         .as_str()
         .unwrap_or_default()
@@ -228,15 +214,20 @@ pub async fn rss_receive(
         .split("&subgroupid=");
     let ani_id = split_ani_sub
         .next()
-        .ok_or("can not found ani_id!")?
+        .ok_or_else(|| anyhow::Error::msg("can not found ani_id!"))?
         .to_string();
     let sub_id = split_ani_sub
         .next()
-        .ok_or("can not found sub_id!")?
+        .ok_or_else(|| anyhow::Error::msg("can not found sub_id!"))?
         .to_string();
-    let sub_name = get_subgroup_name(latest_item.link().ok_or("can not found link!")?, client)
-        .await
-        .unwrap_or_default();
+    let sub_name = get_subgroup_name(
+        latest_item
+            .link()
+            .ok_or_else(|| anyhow::Error::msg("can not found link!"))?,
+        client,
+    )
+    .await
+    .unwrap_or_default();
     let bangumi_name = rss_content["title"]
         .as_str()
         .unwrap_or_default()
@@ -245,52 +236,54 @@ pub async fn rss_receive(
         .unwrap_or(rss_content["title"].as_str().unwrap_or_default())
         .to_string();
     let title = format!("[{sub_name}] {bangumi_name}");
-    let latest_update = latest_item.torrent().ok_or("can not found pubDate!")?["pubDate"]
+    let latest_update = latest_item
+        .torrent()
+        .ok_or_else(|| anyhow::Error::msg("can not found pubDate!"))?["pubDate"]
         .as_str()
-        .ok_or("can not found pubDate!")?
+        .ok_or_else(|| anyhow::Error::msg("can not found pubDate!"))?
         .to_string();
     let bangumi_id = format!("{ani_id}&{sub_id}");
     // check if the bangumi updates and is it first time to be added
     let old_bangumi_dict = old_config["bangumi"].as_object().unwrap();
     let mut magnet_links: Vec<String> = Vec::new();
     if !old_bangumi_dict.contains_key(&bangumi_id) {
-        // TODO: download_all_episode(ani_id, sub_id, title)
         // write to config
         let msg = Message::new(
             vec!["bangumi".to_string(), format!("{ani_id}&{sub_id}")],
             MessageType::Text(latest_update),
             MessageCmd::Replace,
+            None,
         );
         tx.send(msg)?;
-        match get_all_episode_magnet_links(&ani_id, &sub_id, &old_config["filter"]).await {
-            Some(links) => magnet_links = links,
-            None => (),
+        if let Some(links) =
+            get_all_episode_magnet_links(&ani_id, &sub_id, &old_config["filter"]).await
+        {
+            magnet_links = links
         }
         let msg = Message::new(
             vec!["rss_links".to_string(), title.to_string()],
             MessageType::Text(url.to_owned()),
             MessageCmd::Replace,
+            None,
         );
         tx.send(msg)?;
     } else if latest_update == old_bangumi_dict[&bangumi_id] {
         println!("{title} 无更新");
     } else {
-        // println!("waiting...");
-        // tokio::time::sleep(Duration::from_secs(5)).await;
         let mut item_iter = rss_content["item"]
             .as_array()
-            .ok_or("can not find item in rss!")?
+            .ok_or_else(|| anyhow::Error::msg("can not find item in rss!"))?
             .iter()
             .rev();
         for item in &mut item_iter {
             let pub_date = item["torrent"]["pubDate"]
                 .as_str()
-                .ok_or("can not found pub_date!")?;
+                .ok_or_else(|| anyhow::Error::msg("can not found pub_date!"))?;
             if pub_date == old_bangumi_dict[&bangumi_id] {
                 break;
             }
         }
-        let new_items = item_iter.map(|item| item.clone()).collect::<Vec<_>>();
+        let new_items = item_iter.cloned().collect::<Vec<_>>();
         println!("获取到以下剧集：");
         let filter = &old_config["filter"];
         let item_links = filter_episode(&new_items, filter, &sub_id);
@@ -300,19 +293,26 @@ pub async fn rss_receive(
                 eprintln!("Error: {:?}", error);
                 *ERROR_STATUS.write().await = true;
                 drop(TX.write().await.take());
-                return Err("Can not get magnet links!".into());
+                return Err(anyhow::anyhow!("Can not get magnet links!"));
             }
         };
         let msg = Message::new(
             vec!["bangumi".to_string(), format!("{ani_id}&{sub_id}")],
             MessageType::Text(latest_update),
             MessageCmd::Replace,
+            None,
         );
         tx.send(msg)?;
     }
+    if let Some(magnets) = old_config["magnets"][&title].as_array() {
+        magnet_links.append(
+            &mut magnets
+                .iter()
+                .map(|i| i.as_str().unwrap().to_string())
+                .collect::<Vec<_>>(),
+        );
+    }
     if !magnet_links.is_empty() {
-        // println!("waiting...");
-        // tokio::time::sleep(Duration::from_secs(6)).await;
         match cloud_download(&magnet_links).await {
             Ok(hash_list) => {
                 let mut hash_ani = serde_json::Map::new();
@@ -323,14 +323,26 @@ pub async fn rss_receive(
                     vec!["hash_ani".to_string()],
                     MessageType::Map(hash_ani),
                     MessageCmd::Append,
+                    None,
                 );
                 tx.send(msg).unwrap();
+                let msg = Message::new(
+                    vec!["magnets".to_string(), title.to_string()],
+                    MessageType::List(vec![]),
+                    MessageCmd::Replace,
+                    None,
+                );
+                tx.send(msg).unwrap();
+                let notify = Arc::new(Notify::new());
                 let msg = Message::new(
                     vec!["downloading_hash".to_string()],
                     MessageType::List(hash_list),
                     MessageCmd::Append,
+                    Some(notify.clone()),
                 );
                 tx.send(msg).expect("can not send to config thread!");
+                notify.notified().await;
+                restart_refresh_download().await;
             }
             Err(error) => {
                 eprintln!("Error: {:?}", error);
@@ -338,17 +350,14 @@ pub async fn rss_receive(
                     vec!["magnets".to_string(), title.to_string()],
                     MessageType::List(magnet_links),
                     MessageCmd::Append,
+                    None,
                 );
                 tx.send(msg).expect("can not send to config thread!");
                 *ERROR_STATUS.write().await = true;
                 drop(TX.write().await.take());
-                return Err("Can not add magnet to cloud!".into());
+                return Err(anyhow::anyhow!("Can not add magnet to cloud!"));
             }
         }
     }
-    // println!("{} {} {} {:?}", ani_id, sub_id, title, last_update);
-    // for i in rss_content.into_items() {
-    //     println!("{}", &i.title().expect("MUST HAVE TITLE!"));
-    // }
     Ok(())
 }
