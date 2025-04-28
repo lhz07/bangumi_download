c5e447fa40dc4446f4370056a5ad16e3a929d04f
lhz <a2722764368@gmail.com>
2025-04-28 10:14:02 +0800

    finish refresh_download
    fix bugs, add function to restart refresh_download safely

--- src/main_proc.rs
+++ src/main_proc.rs
@@ -1,13 +1,7 @@
-use std::{time::Duration, vec};
-
-use serde_json::Value;
+use std::{collections::HashMap, time::Duration, vec};
 
 use crate::{
-    TX,
-    alist_manager::{get_alist_name_passwd, get_alist_token},
-    cloud_manager::get_tasks_list,
-    config_manager::{CONFIG, Message, MessageCmd, MessageType},
-    update_rss::start_rss_receive,
+    alist_manager::{download_a_task, get_alist_name_passwd, get_alist_token, get_file_raw_url}, cloud_manager::{del_cloud_task, get_tasks_list}, config_manager::{Message, MessageCmd, MessageType, CONFIG}, update_rss::start_rss_receive, REFRESH_DOWNLOAD, TX
 };
 
 pub async fn refresh_rss() {
@@ -23,10 +17,10 @@ pub async fn refresh_rss() {
             .unwrap()
             .to_string();
         let urls = rss_links
-            .as_array()
+            .as_object()
             .unwrap()
             .iter()
-            .map(|link| link.as_str().unwrap())
+            .map(|(_, link)| link.as_str().unwrap())
             .collect::<Vec<_>>();
         start_rss_receive(urls).await;
         println!("\nCheck finished!\n");
@@ -47,12 +41,14 @@ pub async fn refresh_rss() {
                     vec!["user".to_string(), "name".to_string()],
                     MessageType::Text(name),
                     MessageCmd::Replace,
+                    None,
                 );
                 tx.send(msg).unwrap();
                 let msg = Message::new(
                     vec!["user".to_string(), "password".to_string()],
                     MessageType::Text(password),
                     MessageCmd::Replace,
+                    None,
                 );
                 tx.send(msg).unwrap();
             }
@@ -61,7 +57,9 @@ pub async fn refresh_rss() {
 }
 
 pub async fn refresh_download() {
-    loop {
+    let mut wait_time_list = [30, 60, 120, 300, 600, 1200, 1800].into_iter();
+    let mut error_task = HashMap::new();
+    'outer: loop {
         if CONFIG.read().await.get_value()["downloading_hash"]
             .as_array()
             .unwrap()
@@ -86,24 +84,52 @@ pub async fn refresh_download() {
                     .as_str()
                     .unwrap()
                     .to_string();
-
+                let path = format!("/115/云下载/{file_name}/{file_name}");
+                if let Err(error) = download_a_task(&path, &ani_name).await {
+                    eprintln!("Can not download a task, error: {}", error);
+                    error_task
+                        .entry(task["info_hash"].as_str().unwrap().to_string())
+                        .and_modify(|times| *times += 1)
+                        .or_insert(1);
+                    if error_task[task["info_hash"].as_str().unwrap()] > 3  {
+                        break 'outer;
+                    }
+                    tokio::time::sleep(Duration::from_secs(60)).await;
+                    continue;
+                }
                 // after download
+                del_cloud_task(task["info_hash"].as_str().unwrap())
+                    .await
+                    .unwrap();
                 {
                     let tx = TX.read().await.clone().unwrap();
                     let msg = Message::new(
                         vec!["downloading_hash".to_string()],
                         MessageType::Text(task["info_hash"].as_str().unwrap().to_string()),
                         MessageCmd::DeleteValue,
+                        None,
                     );
                     tx.send(msg).unwrap();
                     let msg = Message::new(
                         vec!["hash_ani".to_string()],
                         MessageType::Text(task["info_hash"].as_str().unwrap().to_string()),
                         MessageCmd::DeleteKey,
+                        None,
                     );
                     tx.send(msg).unwrap();
                 }
             }
         }
+        match wait_time_list.next() {
+            Some(secs) => tokio::time::sleep(Duration::from_secs(secs)).await,
+            None => tokio::time::sleep(Duration::from_secs(3600)).await,
+        }
+    }
+}
+
+pub async fn restart_refresh_download(){
+    if let Some(_) = REFRESH_DOWNLOAD.lock().await.take_if(|h|h.is_finished()) {
+        let download_handle = tokio::spawn(refresh_download());
+        REFRESH_DOWNLOAD.lock().await.replace(download_handle);
     }
 }
